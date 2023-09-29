0d46a21878a306b24d85ef92d48c0607c56d9eeb
misael aguayo <aguayo.misael@gmail.com>
2023-09-29 18:07:25 -0500

    Dependency Injection - Example with direct dependency

---
+++ src/MessageWriter.cs
@@ -0,0 +1,7 @@
+public class MessageWriter
+{
+    public void Write(string message)
+    {
+        Console.WriteLine($"MessageWriter.Write(message :\"{message}\")");
+    }
+}
---
+++ src/Worker.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.Hosting;
+
+public class Worker : BackgroundService
+{
+    private readonly MessageWriter _messageWriter = new MessageWriter();
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _messageWriter.Write($"Worker running at: {DateTimeOffset.Now}");
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+
+
+}
