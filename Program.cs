777b4deddd437b032a8809da0756fe83963c2062
misael aguayo <aguayo.misael@gmail.com>
2023-09-29 23:46:49 -0500

    Dependency Injection - Improved logging with built in function

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using DependencyInjection.Example;
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddHostedService<Worker>();
-builder.Services.AddSingleton<IMessageWriter, MessageWriter>();
+builder.Services.AddSingleton<IMessageWriter, LoggingMessageWriter>();
 
 using IHost host = builder.Build();
 host.Run();
---
+++ src/LoggingMessageWriter.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.Logging;
+
+namespace DependencyInjection.Example;
+
+public class LoggingMessageWriter : IMessageWriter
+{
+    private readonly ILogger<LoggingMessageWriter> _logger;
+
+    public LoggingMessageWriter(ILogger<LoggingMessageWriter> logger) =>
+        _logger = logger;
+
+    public void Write(string message) =>
+        _logger.LogInformation("Info: {Msg}", message);
+}
